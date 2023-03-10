9fd2a1f00364c15a5839e077091604be940c4928
Marcin Benka <marcin.benka@gmail.com>
2023-03-10 22:50:45 +0100

    avr: Update and adapt

--- avr/build-avr.sh
+++ avr/build-avr.sh
@@ -8,13 +8,13 @@ source ../common.sh
 #
 
 # package versions
-LIBC_VER="2.1.0"
-#LIBC_VER="svn@svn://svn.savannah.nongnu.org/avr-libc/trunk/avr-libc@2548@avr-libc"
-AVRDUDE_VER="6.4"
+#LIBC_VER="2.1.0"
+LIBC_VER="git@https://github.com/avrdudes/avr-libc.git@55e8cac69935657bcd3e4d938750960c757844c3@main"
+AVRDUDE_VER="7.0"
 
 # download addresses
-LIBC_DNADR="http://download.savannah.gnu.org/releases/avr-libc/avr-libc-${LIBC_VER}.tar.bz2"
-#LIBC_DNADR="$LIBC_VER"
+#LIBC_DNADR="http://download.savannah.gnu.org/releases/avr-libc/avr-libc-${LIBC_VER}.tar.bz2"
+LIBC_DNADR="$LIBC_VER"
 AVRDUDE_DNADR="http://download.savannah.gnu.org/releases/avrdude/avrdude-${AVRDUDE_VER}.tar.gz"
 
 ALL_DNADR+="$LIBC_DNADR $AVRDUDE_DNADR"
@@ -50,38 +50,36 @@ function stage_binutils()
 
 function stage_gcc()
 {
+    set_buildflags_base
     cd ${BUILDDIR}/build-gcc
+
     configure_gcc --with-avrlibc=yes --with-dwarf2 || die "gcc configuration failed..."
     run_make || die "gcc make failed..."
-    run_make -j1 install || die "gcc installation failed..."
+    make -j1 install || die "gcc installation failed..."
 
     remove_bdir build-gcc || die "removing builddir failed..."
 }
 
 function stage_avr_libc()
 {
+    set_buildflags_base
     cd ${BUILDDIR}/build-libc
-    export CFLAGS="${BASE_CFLAGS} -Os -ffunction-sections -fdata-sections"
-    export CXXFLAGS="${CFLAGS}"
-    export LDFLAGS="${BASE_LDFLAGS}"
 
     configure_gen `srcdir ${LIBC_DNADR}` --host=avr --build=`../${SDIR}/config.guess` --with-debug-info=dwarf-2 || die "avr-libc configuration failed..."
     run_make || die "avr-libc make failed..."
-    run_make -j1 install || die "avr-libc installation failed..."
+    make -j1 install || die "avr-libc installation failed..."
 
     remove_bdir build-libc || die "removing builddir failed..."
 }
 
 function stage_avrdude()
 {
+    set_buildflags_base
     cd ${BUILDDIR}/build-avrdude
-    export CFLAGS="${BASE_CFLAGS} -O2"
-    export CXXFLAGS="${CFLAGS}"
-    export LDFLAGS="${BASE_LDFLAGS}"
 
     configure_gen `srcdir ${AVRDUDE_DNADR}` ${CONF_PRFX} --disable-parport || die "avrdude configuration failed..."
     run_make || die "avrdude make failed..."
-    run_make -j1 install || die "avrdude installation failed..."
+    make -j1 install || die "avrdude installation failed..."
 
     remove_bdir build-avrdude || die "removing builddir failed..."
 }
