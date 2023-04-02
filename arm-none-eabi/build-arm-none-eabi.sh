a64e36e01837bcfa69514609d12fcf046046c8c4
Marcin Benka <marcin.benka@gmail.com>
2023-04-02 21:23:37 +0200

    arm-none-eabi: Update newlib to 4.3.0

--- arm-none-eabi/build-arm-none-eabi.sh
+++ arm-none-eabi/build-arm-none-eabi.sh
@@ -8,7 +8,7 @@ source ../common.sh
 #
 
 # package versions
-NEWLIB_VER="4.2.0.20211231"
+NEWLIB_VER="4.3.0.20230120"
 #NEWLIB_VER="git@git://sourceware.org/git/newlib-cygwin.git@8c87ffd372232476ac5d1705dd32ddda54134c2b@master"
 GDB_VER="13.1"
 OPENOCD_VER="0.12.0"
