84a6c14e302f9dbddcaa2b22bee753b3004d5032
Marcin Benka <marcin.benka@gmail.com>
2023-11-15 17:38:52 +0100

    misc: Update gcc-13-2023-11-11, elfutils-0.190

--- VERSIONS.sh
+++ VERSIONS.sh
@@ -3,12 +3,12 @@
 # toolchain main component versions
 BINUTILS_VER="2.41"
 #GCC_VER="13.2.0"
-GCC_VER="13-20230826"
+GCC_VER="git@git://gcc.gnu.org/git/gcc.git@28d9e033a8e95c024e27cb495db610aac37a0f88@releases/gcc-13"
 
 # toolchain package urls
 BINUTILS_DNADR="http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VER}.tar.bz2"
 #GCC_DNADR="http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER}.tar.xz"
-GCC_DNADR="git@git://gcc.gnu.org/git/gcc.git@a9f39860efa645149ec88f0e9a7a31c3362b7e46@releases/gcc-13"
+GCC_DNADR="$GCC_VER"
 
 # prerequisities versions
 ZLIB_VER="1.3"
@@ -17,7 +17,7 @@ MPFR_VER="4.2.1"
 MPC_VER="1.3.1"
 ISL_VER="0.26"
 EXPAT_VER="2.5.0"
-ELFUTILS_VER="0.189"
+ELFUTILS_VER="0.190"
 
 # prerequisities package urls
 ZLIB_DNADR="http://zlib.net/zlib-${ZLIB_VER}.tar.xz"
@@ -26,4 +26,4 @@ MPFR_DNADR="https://www.mpfr.org/mpfr-current/mpfr-${MPFR_VER}.tar.xz"
 MPC_DNADR="https://ftp.gnu.org/gnu/mpc/mpc-${MPC_VER}.tar.gz"
 ISL_DNADR="https://libisl.sourceforge.io/isl-${ISL_VER}.tar.xz"
 EXPAT_DNADR="https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VER//./_}/expat-${EXPAT_VER}.tar.xz"
-ELFUTILS_DNADR="https://sourceware.org/elfutils/ftp/0.189/elfutils-${ELFUTILS_VER}.tar.bz2"
+ELFUTILS_DNADR="https://sourceware.org/elfutils/ftp/${ELFUTILS_VER}/elfutils-${ELFUTILS_VER}.tar.bz2"
