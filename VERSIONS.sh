712c5f1f569b70d186aa84282b2fb353c8a7a447
Marcin Benka <marcin.benka@gmail.com>
2024-06-09 21:06:33 +0200

    misc: Update gcc-14.1.1-2024-06-22

--- VERSIONS.sh
+++ VERSIONS.sh
@@ -3,7 +3,7 @@
 # toolchain main component versions
 BINUTILS_VER="2.42"
 #GCC_VER="14.1.0"
-GCC_VER="git@git://gcc.gnu.org/git/gcc.git@7c49e45fbf845d197b4cb63b3ee2b4ef9214485f@releases/gcc-14" #20240515
+GCC_VER="git@git://gcc.gnu.org/git/gcc.git@70d9d929cee3b6c7313aed62aeb4db7ae3fab59f@releases/gcc-14" #20240622
 #GCC_VER="git@git://gcc.gnu.org/git/gcc.git@a5d2bb333043bda0cc7ba6e36b26205e7f292d40@master"
 
 # toolchain package urls
