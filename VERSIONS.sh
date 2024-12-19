b61cc1e287ea3cbaffe821b9b63b81698257e326
Marcin Benka <marcin.benka@gmail.com>
2024-12-19 13:03:20 +0100

    misc: Update components
      * gcc-14.2.1-2024-12-19
      * expat-2.6.4

--- VERSIONS.sh
+++ VERSIONS.sh
@@ -2,7 +2,7 @@
 BINUTILS_VER="2.43"
 
 #GCC_VER="14.2.0"
-GCC_VER="git@git://gcc.gnu.org/git/gcc.git@e455f398ca0253c73869ced309c9c8b386c06087@releases/gcc-14" #2024-11-03
+GCC_VER="git@git://gcc.gnu.org/git/gcc.git@44da17c12bc4e0e6ded60e927bf38b46638a2594@releases/gcc-14" #2024-12-19
 GCC_DNADR="${GCC_VER}"
 
 # toolchain package urls
@@ -16,5 +16,5 @@ GMP_VER="6.3.0"
 MPFR_VER="4.2.1"
 MPC_VER="1.3.1"
 ISL_VER="0.27"
-EXPAT_VER="2.6.3"
+EXPAT_VER="2.6.4"
 ELFUTILS_VER="0.192"
