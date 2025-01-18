ddc31401202d31aeacb3efe11e12d7253ed578bf
Marcin Benka <marcin.benka@gmail.com>
2025-01-18 18:43:09 +0100

    misc: Update components
      * gcc-14.2.1-2024-01-18

--- VERSIONS.sh
+++ VERSIONS.sh
@@ -2,7 +2,7 @@
 BINUTILS_VER="2.43"
 
 #GCC_VER="14.2.0"
-GCC_VER="git@git://gcc.gnu.org/git/gcc.git@44da17c12bc4e0e6ded60e927bf38b46638a2594@releases/gcc-14" #2024-12-19
+GCC_VER="git@git://gcc.gnu.org/git/gcc.git@511920a21d4096cfc1226e5ec6140551ddc9ca90@releases/gcc-14" #2024-01-18
 GCC_DNADR="${GCC_VER}"
 
 # toolchain package urls
