01486228a51a09c6b4880acf0b63a715747d2d09
Marcin Benka <marcin.benka@gmail.com>
2025-05-31 11:35:50 +0200

    misc: Update components
      * gcc-14.3.1-2025-07-27
      * binutils-2.45
      * elfutils-0.193

--- VERSIONS.sh
+++ VERSIONS.sh
@@ -1,14 +1,13 @@
 # toolchain main component versions
-BINUTILS_VER="2.44"
+BINUTILS_VER="2.45"
 
-#GCC_VER="14.2.0"
-GCC_VER="git@git://gcc.gnu.org/git/gcc.git@04b5c8b90cd95611d99684282cc321f06a0b49c2@releases/gcc-14" #2025-02-28
+#GCC_VER="14.3.0"
+GCC_VER="git@git://gcc.gnu.org/git/gcc.git@7f5b5309ebc5fa66e003950341648d05f6e7062b@releases/gcc-14" #2025-07-27 14.3.x
 GCC_DNADR="${GCC_VER}"
 
 # toolchain package urls
 BINUTILS_DNADR="${BINUTILS_DNADR:=http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VER}.tar.xz}"
 GCC_DNADR="${GCC_DNADR:=http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER}.tar.xz}"
-
 
 # prerequisite versions
 ZLIB_NG_VER="2.2.4"
@@ -18,4 +17,4 @@ MPFR_VER="4.2.2"
 MPC_VER="1.3.1"
 ISL_VER="0.27"
 EXPAT_VER="2.7.1"
-ELFUTILS_VER="0.192"
+ELFUTILS_VER="0.193"
