a1abe5105ced746d347eb6070637c1299da3c921
Marcin Benka <marcin.benka@gmail.com>
2025-08-10 21:09:08 +0200

    misc: Update components
     * zlib-2.2.5
     * gcc-14.3.1-2025-08-10

--- VERSIONS.sh
+++ VERSIONS.sh
@@ -2,7 +2,7 @@
 BINUTILS_VER="2.45"
 
 #GCC_VER="14.3.0"
-GCC_VER="git@git://gcc.gnu.org/git/gcc.git@7f5b5309ebc5fa66e003950341648d05f6e7062b@releases/gcc-14" #2025-07-27 14.3.x
+GCC_VER="git@git://gcc.gnu.org/git/gcc.git@0f8bacc8b2e62d3b81d64ae466ee994d3cab72a4@releases/gcc-14" #2025-08-10 14.3.x
 GCC_DNADR="${GCC_VER}"
 
 # toolchain package urls
@@ -10,7 +10,7 @@ BINUTILS_DNADR="${BINUTILS_DNADR:=http://ftp.gnu.org/gnu/binutils/binutils-${BIN
 GCC_DNADR="${GCC_DNADR:=http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VER}/gcc-${GCC_VER}.tar.xz}"
 
 # prerequisite versions
-ZLIB_NG_VER="2.2.4"
+ZLIB_NG_VER="2.2.5"
 ZSTD_VER="1.5.7"
 GMP_VER="6.3.0"
 MPFR_VER="4.2.2"
