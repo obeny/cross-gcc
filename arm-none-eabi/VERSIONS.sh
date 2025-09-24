d4c15119ec2f117b7ddc77f84a0590c5549659ef
Marcin Benka <marcin.benka@gmail.com>
2025-09-24 20:14:30 +0200

    arm: Update components
     * newlib-4.5.0-cygwin-3.6.5+-2025-09-19
     * openocd-0.12+-artery-2025-09-17

--- arm-none-eabi/VERSIONS.sh
+++ arm-none-eabi/VERSIONS.sh
@@ -3,11 +3,11 @@
 GDB_VER="16.3"
 
 #NEWLIB_VER="4.5.0.20241231"
-NEWLIB_VER="git@git://sourceware.org/git/newlib-cygwin.git@99cfcb824f30aebfd4a9a79f2ee698440f647ea4@cygwin-3_6-branch" #4.5.0+, cygwin-3.6.5+, 2025-07-24
+NEWLIB_VER="git@git://sourceware.org/git/newlib-cygwin.git@275c91f518b9182c94bc33c2788d661f9b6e0a54@cygwin-3_6-branch" #4.5.0+, cygwin-3.6.5+, 2025-09-19
 NEWLIB_DNADR="${NEWLIB_VER}"
 
 #OPENOCD_VER="0.12.0"
-OPENOCD_VER="git@https://github.com/obeny/openocd.git@7541edfcdd6f2ff6b65e313bf17f811f38f3eb2d@master-custom" #0.12+, 2024-11-13
+OPENOCD_VER="git@https://github.com/obeny/openocd.git@31f90d15b8bce5efe05d77d56d79edfd45628b15@master-custom" #0.12+, 2025-09-17
 OPENOCD_DNADR="${OPENOCD_VER}"
 
 # download addresses
